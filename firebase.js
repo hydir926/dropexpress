94d2b4b24f314f97b63f6a32e7e6675820b97791
hydir926 <amirhydir@gmail.com>
2026-03-14 23:17:40 +0000

    Refactor Firebase imports and add storage support
    Removed unused imports and added storage functionality.

--- firebase.js
+++ firebase.js
@@ -1,22 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAs_xRDDAUjh_AemBZFBDCKQ9QXTNWuJ00",
   authDomain: "dropexpress-ba171.firebaseapp.com",
   projectId: "dropexpress-ba171",
   storageBucket: "dropexpress-ba171.firebasestorage.app",
-  messagingSenderId: "1070163070918",
-  appId: "1:1070163070918:web:2c3e26ad6ea0688cb4acc7",
-  measurementId: "G-YSRK4VCL5K"
+  appId: "1:1070163070918:web:2c3e26ad6ea0688cb4acc7"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
 export { 
-    db, auth, collection, getDocs, addDoc, deleteDoc, doc, query, orderBy, onSnapshot,
-    createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut 
+    db, auth, storage, collection, getDocs, addDoc, deleteDoc, doc, query, orderBy, onSnapshot,
+    GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut, ref, uploadBytes, getDownloadURL 
 };
